218ce7a920dfeb009f52ed363843b4cbf06b658d
M. Kozlik <malkak@skyvar.co.il>
2025-03-19 15:36:21 +0200

    Try

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -3,18 +3,18 @@ resource "google_project_service" "cloud_resource_manager"{
   disable_on_destroy = false
 }
 
-resource "google_compute_network" "vpc_network" {
-  name         = var.vpc_name
-  auto_create_subnetworks = false
-  routing_mode = "REGIONAL"
-  depends_on = [ google_project_service.cloud_resource_manager ]
-}
+# resource "google_compute_network" "vpc_network" {
+#   name         = var.vpc_name
+#   auto_create_subnetworks = false
+#   routing_mode = "REGIONAL"
+#   depends_on = [ google_project_service.cloud_resource_manager ]
+# }
 
-resource "google_compute_subnetwork" "subnetwork" {
-  name          = var.subnetwork_name
-  region        = var.region
-  ip_cidr_range = var.ip_cidr_range
-  network       = google_compute_network.vpc_network.id
-  private_ip_google_access = true
-  purpose = "PRIVATE"
-}
+# resource "google_compute_subnetwork" "subnetwork" {
+#   name          = var.subnetwork_name
+#   region        = var.region
+#   ip_cidr_range = var.ip_cidr_range
+#   network       = google_compute_network.vpc_network.id
+#   private_ip_google_access = true
+#   purpose = "PRIVATE"
+# }
