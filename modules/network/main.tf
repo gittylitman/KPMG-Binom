ee8fb3eadd61d85ee12e3825e6bc1c704a9e95a7
Shoshana <shoshig@skyvar.co.il>
2025-03-23 12:20:15 +0200

    Add enable api resource

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,6 +1,17 @@
+resource "google_project_service" "compute" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "time_sleep" "wait_60_seconds" {
+  create_duration = "60s"
+  depends_on = [ google_project_service.compute ]
+}
+
 data "google_compute_network" "vpc_network" {
   name         = var.vpc_name
   project = var.host_project_id
+  depends_on = [ time_sleep.wait_60_seconds ]
 }
 
 data "google_compute_subnetwork" "subnetwork" {
