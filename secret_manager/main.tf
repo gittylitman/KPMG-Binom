a1d220a3c6b826a64689626beb8576523c7ce835
M. Kozlik <malkak@skyvar.co.il>
2025-03-27 12:44:10 +0200

    Add depends_on

--- secret_manager/main.tf
+++ secret_manager/main.tf
@@ -5,17 +5,18 @@ terraform {
   }
 }
 
+provider "google" {
+  project = var.project_id
+}
+
 resource "google_project_service" "secretmanager" {
   service            = "secretmanager.googleapis.com"
   disable_on_destroy = false
-}
-
-provider "google" {
-  project = var.project_id
 }
 
 resource "google_secret_manager_regional_secret" "secret" {
   secret_id = var.secret_id[count.index]
   location = var.location
   count = length(var.secret_id)
+  depends_on = [ google_project_service.secretmanager ]
 }
