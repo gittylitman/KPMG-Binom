2a2a35cec419fc1cdfc7a43bae5dfc740584b828
M. Kozlik <malkak@skyvar.co.il>
2025-03-27 13:04:20 +0200

    ADd

--- secret_manager/variables.tf
+++ secret_manager/variables.tf
@@ -1,11 +1,24 @@
 variable "project_id" {
   type = string
+  default = "binom-project"
+}
+
+variable "project_name" {
+  type = string
+  default = "binom"
+}
+
+variable "environment" {
+  type = string
+  default = "dev"
 }
 
 variable "secret_id" {
   type = list(string)
+  default = [ "certificate", "private_key" ]
 }
 
 variable "location" {
   type = string
+  default = "me-west1"
 }
