f5e49e186c2613d7b89c04a27254a62460187209
M. Kozlik <malkak@skyvar.co.il>
2025-03-27 12:17:43 +0200

    Fill variables in secret manager to check it

--- secret_manager/variables.tf
+++ secret_manager/variables.tf
@@ -1,11 +1,14 @@
 variable "project_id" {
   type = string
+  default = "binom-project"
 }
 
 variable "secret_id" {
   type = list(string)
+  default = [ "certificate", "private_key" ]
 }
 
 variable "location" {
   type = string
+  default = "me-west1"
 }
